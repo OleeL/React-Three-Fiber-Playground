6fa0dd167ba36621fda43a54d4bf91f73f058b81
Oli Legg <oliglegg@hotmail.co.uk>
2020-08-06 23:59:05 +0100

    Loading in plane

--- components/game/models/models.tsx
+++ components/game/models/models.tsx
@@ -5,9 +5,9 @@ import GLTF from "./model-helpers/GLTF"
 const Models: FC = () => 
     <Suspense fallback={<Box />}>
         <GLTF
-            name={"RetroChair"}
+            name={"embPlane"}
             position={[0, -1.33, 0]}
-            scale={[0.02, 0.02, 0.02]}
+            scale={[0.2, 0.2, 0.2]}
             rotation={[1.5708, 0, 0]}
             castShadow
             receiveShadow
